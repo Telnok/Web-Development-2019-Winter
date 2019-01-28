8528d524af5282fc079ccb26705e203221191585
Gerald Aden <gerald@gmatech.com>
2019-01-28 08:18:14 -0800

    Added Docker ignore and JS samples

---
+++ Examples/Week3/samples/jssamples.js
@@ -0,0 +1,18 @@
+var name = "Gerald";
+var score = 80;
+var completed = true;
+
+console.log(name + " score is " + score);
+console.log(name + " has completed: " + completed + '\n');
+
+if (score > 90) {
+    console.log("Great job!!!\n");
+} else {
+    console.log("Keep working hard!!!\n");
+}
+
+var names = ['Bob', 'Tom', 'Bill'];
+
+for (i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
