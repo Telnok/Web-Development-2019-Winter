05a00c988c265aa5c980efd218f1d41eb5d8600f
Gerald Aden <gerald@gmatech.com>
2019-02-04 10:52:15 -0800

    Added example Vue app

---
+++ Examples/Week4/app1/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+
+    <div><img src="./assets/logo.jpg"></div>
+
+    <div class="menu-bar">
+        <span class="menu-item" v-on:click='goToHome'>Home</span>
+        <span class="menu-item" v-on:click='goToRegistration'>Registration</span>
+        <span class="menu-item" v-on:click='goToProducts'>Products</span>
+    </div>
+
+    <router-view/>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    methods: {
+      goToHome: function() {
+        this.$router.push({ path: 'home' })
+      },
+      goToRegistration: function() {
+        this.$router.push({ path: 'registration' })
+      },
+      goToProducts: function() {
+        this.$router.push({ path: 'products' })
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 20px;
+  }
+
+  .menu-item {
+      margin-right: 10px;
+      padding: 5px;
+  }
+
+  .menu-bar {
+      background-color: cyan;
+      height: 40px;
+      margin-top: 10px;
+      vertical-align: middle;
+      padding-top: 20px;
+      width: 50%;
+  }
+</style>
---
+++ Examples/Week4/app1/src/components/Registration.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Registration</h1>
+        <form>
+            <div class='form-entry'>
+                First Name: <input type='text'/>
+            </div>
+
+            <div class='form-entry'>
+                Last Name: <input type='text'/>
+            </div>
+
+            <div class='form-entry'>
+                <input type='button' value='Submit'/>
+            </div>
+        </form>
+
+        <!-- <div id='error'></div>
+
+        <div class="menu-bar">
+            <span class="menu-item"><a href="index.html">Home</a></span>
+            <span class="menu-item"><a href="products.html">Products</a></span>
+        </div> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Registration',
+        data () {
+            return {
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
