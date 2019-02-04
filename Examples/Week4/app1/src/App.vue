451a4d3f177aae019acaabbdf66110001d86ccb0
Gerald Aden <gerald@gmatech.com>
2019-02-04 11:59:55 -0800

    Refactored navbar into separate component

--- Examples/Week4/app1/src/App.vue
+++ Examples/Week4/app1/src/App.vue
@@ -3,11 +3,7 @@
 
     <div><img src="./assets/logo.jpg"></div>
 
-    <div class="menu-bar">
-        <span class="menu-item" v-on:click='goToHome'>Home</span>
-        <span class="menu-item" v-on:click='goToRegistration'>Registration</span>
-        <span class="menu-item" v-on:click='goToProducts'>Products</span>
-    </div>
+    <NavBar></NavBar>
 
     <router-view/>
 
@@ -15,19 +11,11 @@
 </template>
 
 <script>
+  import NavBar from './components/NavBar.vue';
+  
   export default {
     name: 'App',
-    methods: {
-      goToHome: function() {
-        this.$router.push({ path: 'home' })
-      },
-      goToRegistration: function() {
-        this.$router.push({ path: 'registration' })
-      },
-      goToProducts: function() {
-        this.$router.push({ path: 'products' })
-      }
-    }
+    components: { NavBar },
   }
 </script>
 
@@ -38,19 +26,5 @@
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
     margin-top: 20px;
-  }
-
-  .menu-item {
-      margin-right: 10px;
-      padding: 5px;
-  }
-
-  .menu-bar {
-      background-color: cyan;
-      height: 40px;
-      margin-top: 10px;
-      vertical-align: middle;
-      padding-top: 20px;
-      width: 50%;
   }
 </style>
---
+++ Examples/Week4/app1/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="menu-bar">
+        <span class="menu-item" v-on:click='goToHome'>Home</span>
+        <span class="menu-item" v-on:click='goToRegistration'>Registration</span>
+        <span class="menu-item" v-on:click='goToProducts'>Products</span>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+    methods: {
+      goToHome: function() {
+        this.$router.push({ path: 'home' })
+      },
+      goToRegistration: function() {
+        this.$router.push({ path: 'registration' })
+      },
+      goToProducts: function() {
+        this.$router.push({ path: 'products' })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .menu-item {
+        margin-right: 10px;
+        padding: 5px;
+    }
+
+    .menu-bar {
+        background-color: cyan;
+        height: 40px;
+        margin-top: 10px;
+        vertical-align: middle;
+        padding-top: 20px;
+        width: 50%;
+    }
+</style>
