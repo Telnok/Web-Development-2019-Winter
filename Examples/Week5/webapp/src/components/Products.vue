712fa7e9c5119292cfdf7fc9986ba7b8552f204a
Gerald Aden <gerald@gmatech.com>
2019-02-05 11:16:34 -0800

    Start of week 5 example

---
+++ Examples/Week5/webapp/src/components/Products.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2 class='section-heading'>Products</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Available</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="product in products">
+                    <td>{{ product.name }}</td>
+                    <td>{{ product.price }}</td>
+                    <td>{{ product.available }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Products',
+        mounted() {
+            this.products = getProducts();
+        },
+        data () {
+            return {
+                products: []
+            }
+        }
+    }
+
+    function getProducts() {
+        return JSON.parse(testProducts).products;
+    }
+
+    var testProducts = '{"products": [{"name": "television","price": 299.00,"available": true},{"name": "recliner","price": 199.00,"available": false}]}'
+</script>
+
+<style scoped>
+</style>
